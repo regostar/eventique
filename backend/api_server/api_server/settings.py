872bb9f3da8f8d093c080ea539cc103d254047ac
renugopal <renugopal17@hotmail.com>
2024-03-22 14:48:29 -0500

    Add app for user authentication - create model using base User model from django - create endpoints for login, signup, logout and user details - add validations - add sample User view which checks if user is authenticated

--- backend/api_server/api_server/settings.py
+++ backend/api_server/api_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
+    'user_auth_api',
     'dashboard',
 ]
 
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'api_server.urls'
@@ -128,3 +132,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+AUTH_USER_MODEL = 'user_auth_api.AppUser'
+
+
+# CORS_ORIGIN_ALLOW_ALL = True
+# enable only in local
--- backend/api_server/api_server/urls.py
+++ backend/api_server/api_server/urls.py
@@ -19,30 +19,16 @@ from django.urls import include, path
 from rest_framework import routers
 from dashboard import views
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    
+    path('api/', include('user_auth_api.urls')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
-
-
-
-
-
-
-
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
