60055cf30aefe7368219a9e52f812d7b381440ff
Sparsha Prakash <sparshadp103@gmail.com>
2024-03-30 22:58:44 -0500

    Homepage Basic Layout without the Calendar- 54

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import Sidebar from './Sidebar'; 
+=======
+import Sidebar from './Sidebar';
+import { CgProfile } from "react-icons/cg";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FaPlus } from "react-icons/fa";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="relative">
       <div className="absolute top-0 right-0">
-        <div className="flex items-center space-x-4 mt-4 mr-4"> { }
-          <div className="bg-purple-600 text-white px-4 py-2 font-bold">
-            + Create
+        <div className="flex items-center space-x-4 mt-4 mr-4">
+          <div className="bg-purple-600 text-white px-4 py-2 font-bold flex items-center">
+            <FaPlus className="mr-1" />
+            <span>Create</span>
           </div>
-          <div className="profile-icon">
-            {/* Inserting the profile icon */}
-            <img src="profile-icon.png" alt="Profile Icon" className="w-8 h-8 rounded-full" />
+          <div className="inline-block">
+            <CgProfile className="w-8 h-8 mr-0.5" />
           </div>
-          <div className="down-arrow-icon">
-            {/* Inserting the down arrow icon*/}
-            <img src={process.env.PUBLIC_URL + '/down_arrow.png'} alt="down-arrow-icon" className="w-4 h-4 mt-1 mr-2" /> { }
+          <div className="inline-block">
+            <IoMdArrowDropdown className="w-6 h-6 mt-1 mr-6" />
           </div>
         </div>
       </div>
-      <div className="app-container">
+      <div className="flex items-start">
         <Sidebar />
+        <span className="text-black px-4 py-2 font-bold ml-2 mt-4 text-2xl">
+          Calendar
+        </span>
       </div>
     </div>
   );
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { IoCalendarOutline } from "react-icons/io5";
+
+import eventique_logo from '../assets/login/logo.jpg';
+import sidebar_image from '../assets/Homepage/sidebar_image.png';
 
 function Sidebar() {
   return (
-    <div className="sidebar bg-gray-200 w-64 h-screen flex flex-col">
-      <div className="sidebar-content flex items-center justify-start mt-10"> { }
-        <img className="w-15 h-15 ml-2 mr-2" src={'./eventique_logo.jpg'} alt="Your Logo" /> { }
-        <span className="sidebar-text font-bold text-xl">Eventique</span> { }
+    <div className="bg-gray-200 w-64 h-screen flex flex-col">
+      <div className="flex items-center justify-start mt-10">
+        <img className="w-12 h-12 ml-2 mr-2" src={eventique_logo} alt="Your Logo" />
+        <span className="font-bold text-xl text-gray-800">Eventique</span>
       </div>
-      <div className="sidebar-item bg-purple-600 text-white py-2 px-4 font-bold mt-4 text-center relative flex justify-center items-center">
-        <img className="w-6 h-6 mr-2" src={'/calendar2_icon.png'} alt="Calendar Icon" />
-        <span className="sidebar-calendar" style={{ top: '-5px' }}>Calendar</span> { }
+      <div className="bg-purple-600 text-white py-2 px-4 font-bold mt-4 text-center relative flex justify-center items-center">
+        <IoCalendarOutline className="w-6 h-6 mr-2" />
+        <span className="text-lg">Calendar</span>
       </div>
-      <div className="sidebar-image-container mt-auto">
-        <img src={'/sidebar_image.png'} alt="Sidebar Image" className="w-full" />
+      <div className="mt-auto flex justify-center">
+        <img src={sidebar_image} alt="Sidebar Image" className="w-full" />
       </div>
     </div>
   );
