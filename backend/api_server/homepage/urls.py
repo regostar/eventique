97cc29ab8f37678e676882c5c141e39354864389
renugopal <renugopal17@hotmail.com>
2024-03-31 20:21:14 -0500

    update tests bug fix - view urls mistaken bug fix - views - json parsing

--- backend/api_server/homepage/urls.py
+++ backend/api_server/homepage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('event/new/', views.get_event, name='get_event'),
-    path('event/create/', views.get_event, name='get_event'),
+    path('event/create/', views.create_event, name='create_event'),
 ]
