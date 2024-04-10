b32c890983ef3b695de1ecb3d09417edc9c5e773
Govind-Asawa <govindasawa23@gmail.com>
2024-04-10 15:49:53 -0500

    Made chatpage modular, added PromptSuggestion component ensuring consistent UI

--- frontend/src/components/Chatpage.jsx
+++ frontend/src/components/Chatpage.jsx
@@ -1,48 +1,51 @@
 import React, { useState } from 'react';
 import { RiQuestionMark, RiSendPlaneFill } from 'react-icons/ri';
 import { BiPaint } from 'react-icons/bi';
+import { PiLightbulb } from "react-icons/pi";
 
+import PromptSuggestion from './PromptSuggestion';
+
+const iconCSS = 'w-6 h-6 text-[#9BD61DFF]';
 const suggestions = [
-  { display: 'Plan for birthday party', icon: <RiQuestionMark /> },
+  { display: 'Plan for birthday party', icon: <RiQuestionMark className={iconCSS}/> },
   {
     display: 'create a roadmap to host a small formal meetup',
-    icon: <BiPaint />,
+    icon: <BiPaint className={iconCSS}/>,
+  },
+  {
+    display: 'I wish to surprise my in-laws coming this weekend, what do I do?',
+    icon: <PiLightbulb className={iconCSS}/>,
   },
 ];
 
 export default function Chatpage() {
-  const [prompt, setPrompt] = useState('')
+  const [prompt, setPrompt] = useState('');
   return (
     <>
       <div className='flex flex-col gap-5 h-full justify-start items-center px-4'>
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col w-full gap-3'>
           <div className='flex flex-col px-4 py-2 items-center justify-center'>
             <p className='text-3xl text-gray-800 font-bold'>
               Go Ahead plan your Event!
             </p>
           </div>
-          <div className='flex flex-col w-full gap-1 items-center justify-center'>
-            {suggestions.map((option) => {
-              return (
-                <div className='flex justify-center gap-2 items-center p-2'>
-                  {option.icon}
-                  <p>{option.display}</p>
-                </div>
-              );
-            })}
+          <div className='flex flex-col w-full px-4 gap-1 justify-items-start justify-center'>
+            {suggestions.map((option, i) => (
+              <PromptSuggestion key={i} option={option} setPrompt={setPrompt} />
+            ))}
           </div>
         </div>
-          <div className='flex flex-1 absolute bottom-0 justify-center items-center w-full mt-auto p-2 border-2 rounded-lg border-purple-200 focus:shadow-md'>
-            <input
-              type='text'
-              className='outline-none p-2 flex-1'
-              placeholder='Describe your event...'
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
-            <RiSendPlaneFill className='w-9 h-9 p-2' />
-          </div>
+        <div className='flex flex-1 absolute bottom-2 justify-center items-center w-full mt-auto p-2 border-2 rounded-lg border-purple-200 focus-within:shadow-md'>
+          <input
+            type='text'
+            className='outline-none p-2 flex-1'
+            placeholder='Describe your event...'
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <RiSendPlaneFill className='w-9 h-9 p-2 cursor-pointer'/>
         </div>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/PromptSuggestion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PromptSuggestion({ key, option, setPrompt }) {
+  return (
+    <div
+      key={key}
+      className='flex justify-start p-1 border-2 border-gray-100 hover:border-gray-300 hover:cursor-pointer rounded-md gap-2 items-center p-2'
+      onClick={(e) => setPrompt(option.display)}
+    >
+      <div className='flex justify-center bg-[#F6FCE8FF] p-4 rounded-sm'>{option.icon}</div>
+      <p>{option.display}</p>
+    </div>
+  );
+}
