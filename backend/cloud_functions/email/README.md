76e06f3649071937b836142e8cb97a71633cae52
renugopal <renugopal17@hotmail.com>
2024-04-19 21:23:40 -0500

    GCF function made generic

--- backend/cloud_functions/email/README.md
+++ backend/cloud_functions/email/README.md
@@ -1,4 +1,8 @@
 gcloud functions deploy send_email --runtime python39 --trigger-http --allow-unauthenticated --set-env-vars SENDGRID_API_KEY='YOUR_SENDGRID_API_KEY'
 
+Create a queue - task
+
+gcloud tasks queues create email-queue --location=us-central1
+
 
 
--- backend/cloud_functions/email/main.py
+++ backend/cloud_functions/email/main.py
@@ -11,19 +11,13 @@ from sendgrid.helpers.mail import Mail
 # Demo function
 def send_email(request):
     """HTTP Cloud Function to be triggered by HTTP."""
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    data = request.get_json() or {}
+    recipient_email = data.get('recipient', recipient_email)
+    subject = data.get('subject', subject)
+    content = data.get('content', content)
 
     sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
-    sender_email = 'regostar007@gmail.com'  # This should be a verified sender email in SendGrid
-    recipient_email = 'regostar006@gmail.com'
-    subject = 'Hello from Google Cloud Function!'
-    content = 'This email is sent from a Google Cloud Function using SendGrid.'
-
-    if request_json and 'recipient' in request_json:
-        recipient_email = request_json['recipient']
-    elif request_args and 'recipient' in request_args:
-        recipient_email = request_args['recipient']
+    sender_email = os.getenv('SENDER_EMAIL')
 
     message = Mail(
         from_email=sender_email,
