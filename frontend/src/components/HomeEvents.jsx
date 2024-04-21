3a7580a29c2aad0a6ed4f6af99a7dceddf8a2bc9
Govind-Asawa <govindasawa23@gmail.com>
2024-04-20 19:15:27 -0500

    Updated styling, optimized logic for checking event  status. Moved test data to testData.js file. tested req/resp

--- frontend/src/components/HomeEvents.jsx
+++ frontend/src/components/HomeEvents.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Table from './Table'; 
+import Table from './Table';
 import { apiEndpoints } from '../utils/apiEndpoints';
-import Loader from './Loader'; 
+import Loader from './Loader';
+
+import { events as testEvents } from '../testData';
 
 export default function HomeEvents() {
   const [events, setEvents] = useState([]);
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
         setLoading(true); // setting loading to true before fetching the data
         const response = await axios.get(apiEndpoints.GET_EVENTS);
-        setEvents(response.data); 
+        if (response?.data?.events)
+          setEvents(response.data.events);
+        // setEvents([ ...testEvents ]);
       } catch (error) {
-        console.error("Error fetching events:", error);
+        console.error('Error fetching events:', error);
       } finally {
         setLoading(false); // setting loading to false after the data is fetched
       }
@@ -27,7 +31,7 @@ export default function HomeEvents() {
   return (
     <div>
       {loading ? (
-        <Loader message="Loading..." /> // adding the loader message
+        <Loader message='Loading...' /> // adding the loader message
       ) : (
         <>
           <Table events={events} showHeadings={true} />
