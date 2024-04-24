ce3855bfc9f8020672bcebd843dbfcc75dc64512
Sparsha Prakash <sparshadp103@gmail.com>
2024-04-24 12:31:50 -0500

    Added event details- basic structure-47

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import moment from 'moment';
 
 import { apiEndpoints } from '../utils/apiEndpoints';
 import Loader from './Loader';
@@ -34,7 +35,33 @@ export default function Event() {
 
     return (
         <div>
-            Event details go here ...
-        </div>
-    )
+            {/* Event Title */}
+            <h2>{event ? event.title : 'Event Title'}</h2>
+        
+            {/* Start and End Dates */}
+            <p>
+                {event ? (
+                    <>
+                        Start Date: {moment(event.startDate).format('MMMM Do YYYY, h:mm a')}<br />
+                        End Date: {moment(event.endDate).format('MMMM Do YYYY, h:mm a')}
+                    </>
+                ) : (
+                    'Event Dates'
+                )}
+            </p>
+        
+            {/* Event Description */}
+            <p>{event ? event.description : 'Event Description'}</p>
+        
+            {/* List of Tasks */}
+            <div>
+                <h3>Tasks:</h3>
+                {/* Dummy task for now */}
+                <p>
+                    {/* Need to add navigation to the page that shows each task in detail */}
+                    <a href="#">Task 1</a>
+                </p>
+            </div>
+        </div> 
+    );
 }
