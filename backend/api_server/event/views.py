535d13f09c109026ce9d54a7518bb211fc02ee64
Govind-Asawa <govindasawa23@gmail.com>
2024-04-29 12:58:43 -0500

    Added endpoint for single event. Only returning event details. Tasks not included

--- backend/api_server/event/views.py
+++ backend/api_server/event/views.py
@@ -36,3 +36,28 @@ def get_events(request):
     except Exception as e:
         print("Error - ", str(e))
         return JsonResponse({'error': str(e)}, status=500)
+
+@require_http_methods(["GET"])
+def single_event(request, eventId=None):
+    try:
+        event_obj = Event.objects.get(pk=eventId)
+    except Event.DoesNotExist:
+        return JsonResponse({'error': "No valid Event found"}, status=404)
+    
+    try:
+        if request.method == 'GET':
+            jsonEvent = {
+                'id': event_obj.id,
+                'title': event_obj.title,
+                'start': event_obj.start_time,
+                'end': event_obj.end_time,
+                'description': event_obj.description,
+            }
+
+            return JsonResponse(jsonEvent, status=200)
+    
+    except Exception as e:
+        print("Error - ", str(e))
+        return JsonResponse({'error': str(e)}, status=500)
+        
+
